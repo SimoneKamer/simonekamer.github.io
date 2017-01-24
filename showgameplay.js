e8417e4bd631ca6ef63a02381ed21d58e964ed96
Simone-laptop\Simone <simone.kamer@gmail.com>
2017-01-24 22:19:46 +0100

    tried to fugure out how to get it communicating with memory.js stopped because of tired head...

--- showgameplay.js
+++ showgameplay.js
@@ -1,29 +1,36 @@
-function ShowGamePlay (){
-}
+function ShowGamePlay (position){
+var position;
+var showGameArea;
+var showPositionStatus;
+var showScore;
 
-function showPositionStatus (position){
+    this.showPositionStatus = function(position){
         if ((position.isOccupied()){
             if (position.getCard().isVisible()){
-            console.log ("voorkant" /*position.getCard().getCardName()*/);
-            else console.log ("achterkant");
+            console.log ("-voorkant-" /*position.getCard().getCardName()*/);
+            else console.log ("-achterkant-");
             }
         }
         else {
-        console.log ("leeg");
+        console.log ("-leeg-");
         }
     }
-};
 
-function showGameArea (){
-    for (i=0;i<positions.length;i++){
-        showPositionStatus (positions[i]);
+
+    this.showGameArea = function (){
+        for (i=0;i<positions.length;i++){
+            showPositionStatus (positions[i]);
+        }
     }
-}
 
+};
 
 
 // callActivePlayer
 // showScore
 // showGameArea
-// console.log: ("dit is nu het speelbord: " + showGameArea() );
+ShowGamePlay.prototype.showGameArea function showGameArea() {
+    return this.showGameArea();
+
+    console.log ("dit is nu het speelbord: " + showGameArea() );
 
