251408f0c1fb32857e5b4dcde21987b5b5ac3ef5
Simone-laptop\Simone <simone.kamer@gmail.com>
2017-01-26 22:25:01 +0100

    implemented memory functionality in actual game memory.html

--- visualiser.js
+++ visualiser.js
@@ -8,8 +8,11 @@ Visualiser.prototype.revealCard = function(cardName, index) {
 
 
     // Zoek de juiste position
-    // var position = document.getElementsByClassName("position")[index];
+    var position = document.getElementsByClassName("position")[index];
 
     // Wissel het plaatje van het kaartje
-    // position.getElementsByTagName("img")[0].src = '../simonekamer/images/' + cardName + ".jpg";
+    position.getElementsByTagName("img")[0].src = 'images/' + cardName + ".jpg";
 }
+// hide card
+// empty position
+// show score
